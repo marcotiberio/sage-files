51b8fdd530cc07dccd46101769c98de79bdc117b
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-11-21 15:28:04 +0100

    footer

--- app/setup.php
+++ app/setup.php
@@ -58,6 +58,7 @@ add_action('after_setup_theme', function () {
      */
     register_nav_menus([
         'primary_navigation' => __('Primary Navigation', 'sage'),
+        'footer_navigation' => __('Footer Navigation', 'sage'),
     ]);
 
     /**
--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,39 @@
-<footer class="content-info p-5 bg-black text-white">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="content--info p-4 py-6 mx-auto md:p-8 lg:-10 bg-white text-black">
+  <div class="grid grid-cols-2 gap-8 lg:grid-cols-6">
+      <div class="col-span-2">
+        <div class="description"><?php the_field('descriptionHtml', 'option'); ?></div>
+        <div class="socials flex flex-row mt-5 space-x-6">
+          <?php if( have_rows('social_media', 'options') ): ?>
+            <?php while( have_rows('social_media', 'options') ): the_row();
+                ?>
+                <a class="capitalize mr-sm" href="{{the_sub_field('social_url')}}" target="_blank">{{the_sub_field('social_channel')}}</a>
+            <?php endwhile; ?>
+          <?php endif; ?>
+        </div>
+      </div>
+      <div class="lg:mx-auto">
+        <h2 class="mb-6 text-sm font-semibold uppercase">Company</h2>
+        <nav class="nav-footer text-black" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+          {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
+        </nav>
+      </div>
+      <div class="lg:mx-auto">
+        <h2 class="mb-6 text-sm font-semibold uppercase">Help center</h2>
+        <nav class="nav-footer text-black" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+          {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
+        </nav>
+      </div>
+      <div class="lg:mx-auto">
+        <h2 class="mb-6 text-sm font-semibold uppercase">Legal</h2>
+        <nav class="nav-footer text-black" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+          {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
+        </nav>
+      </div>
+      <div class="lg:mx-auto">
+        <h2 class="mb-6 text-sm font-semibold uppercase">Download</h2>
+        <nav class="nav-footer text-black" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+          {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
+        </nav>
+      </div>
+  </div>
 </footer>
