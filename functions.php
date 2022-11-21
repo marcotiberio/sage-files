4afa3433885e1cd682daf10a9ad35fa8885516f7
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-11-21 15:27:51 +0100

    deactivated comments, clean wp head, options page

--- functions.php
+++ functions.php
@@ -45,19 +45,40 @@
 |--------------------------------------------------------------------------
 */
 
-// add_filter( 'allowed_block_types_all', 'sage_allowed_block_types', 25, 2 );
+add_filter( 'allowed_block_types_all', 'sage_allowed_block_types', 25, 2 );
  
-// function sage_allowed_block_types( $allowed_blocks, $editor_context ) {
+function sage_allowed_block_types( $allowed_blocks, $editor_context ) {
  
-// 	return array(
-// 		'core/image',
-// 		'core/paragraph',
-// 		'core/heading',
-// 		'core/list',
-// 		'acf/testimonial'
-// 	);
+	// return array(
+	// 	'core/image',
+	// 	'core/paragraph',
+	// 	'core/heading',
+	// 	'core/list',
+	// 	'acf/testimonial'
+	// );
  
-// }
+}
+
+/*
+|--------------------------------------------------------------------------
+| Options Page
+|--------------------------------------------------------------------------
+*/
+
+if( function_exists('acf_add_options_page') ) {
+    
+    acf_add_options_page(array(
+        'page_title'    => 'Global Options',
+        'menu_title'    => 'Global Options',
+        'menu_slug'     => 'global-options-settings',
+        'capability'    => 'edit_posts',
+        'parent_slug'   => '',
+        'redirect'      => false,
+        'position'      => false,
+        'icon_url'      => false
+    ));
+    
+}
 
 /*
 |--------------------------------------------------------------------------
@@ -264,6 +285,93 @@ function custom_excerpt_length( $length ) {
 	return 20;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+/*
+|--------------------------------------------------------------------------
+| Deactivate Comments
+|--------------------------------------------------------------------------
+*/
+
+// Removes from admin menu
+add_action('admin_menu', 'my_remove_admin_menus');
+function my_remove_admin_menus()
+{
+    remove_menu_page('edit-comments.php');
+}
+// Removes from post and pages
+add_action('init', 'remove_comment_support', 100);
+
+function remove_comment_support()
+{
+    remove_post_type_support('post', 'comments');
+    remove_post_type_support('page', 'comments');
+}
+// Removes from admin bar
+function mytheme_admin_bar_render()
+{
+    global $wp_admin_bar;
+    $wp_admin_bar->remove_menu('comments');
+}
+add_action('wp_before_admin_bar_render', 'mytheme_admin_bar_render');
+
+/*
+|--------------------------------------------------------------------------
+| Clean up wp_head()
+|--------------------------------------------------------------------------
+|
+| Remove unnecessary <link>'s
+| Remove inline CSS and JS from WP emoji support
+| Remove inline CSS used by Recent Comments widget
+| Remove inline CSS used by posts with galleries
+| Remove self-closing tag
+|
+*/
+
+add_action('init', function () {
+    // Originally from http://wpengineer.com/1438/wordpress-header/
+    remove_action('wp_head', 'feed_links_extra', 3);
+    add_action('wp_head', 'ob_start', 1, 0);
+    add_action('wp_head', function () {
+        $pattern = '/.*' . preg_quote(esc_url(get_feed_link('comments_' . get_default_feed())), '/') . '.*[\r\n]+/';
+        echo preg_replace($pattern, '', ob_get_clean());
+    }, 3, 0);
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10);
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_action('wp_head', 'wp_oembed_add_discovery_links');
+    remove_action('wp_head', 'wp_oembed_add_host_js');
+    remove_action('wp_head', 'rest_output_link_wp_head', 10);
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+    add_filter('use_default_gallery_style', '__return_false');
+    add_filter('emoji_svg_url', '__return_false');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Get File Size
+|--------------------------------------------------------------------------
+*/
+
+function getFileSize($id)
+{
+    $file = get_attached_file($id);
+    if ($file) {
+        $bytes = filesize($file);
+        $s = array('b', 'Kb', 'Mb', 'Gb');
+        $e = floor(log($bytes) / log(1024));
+        return sprintf('%.2f ' . $s[$e], ($bytes / pow(1024, floor($e))));
+    } else {
+        return null;
+    }
+}
 
 /*
 |--------------------------------------------------------------------------
