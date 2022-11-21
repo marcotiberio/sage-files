5a5291594c2f357a60f3216f4fb09be54fafff3a
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-11-21 15:29:06 +0100

    header structure

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,15 +1,37 @@
-<header class="banner p-5 bg-white">
-  <a class="brand text-black text-lg font-bold" href="{{ home_url('/') }}">
+<header class="header flex flex-row justify-between items-center p-5 bg-white shadow-sm">
+  <a class="logo text-black text-lg font-bold" href="{{ home_url('/') }}">
     {!! $siteName !!}
   </a>
 
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary text-black" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
-    </nav>
+  <div class="nav">
+    <div x-data="{ open: false }"  class="nav--desktop">
+      <nav class="nav-primary text-black" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
+      </nav>
+    </div>
+    <div x-data="{ open: false }"  class="nav--mobile">
+      <div x-on:click="open = ! open"  class="hamburger relative h-[18px] w-[30px]">
+        <span class="hamburger-barOne bg-black absolute rounded h-[3px] w-[30px] top-[0px]"></span>
+        <span class="hamburger-barTwoo bg-black absolute rounded h-[3px] w-[30px] top-[9px]"></span>
+        <span class="hamburger-barThree bg-black absolute rounded h-[3px] w-[20px] top-[18px]"></span>
+      </div>
+      <nav x-show="open" class="nav-primary bg-white text-black text-center" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'items_wrap' => '<ul class="nav your_custom_class">%3$s</ul>', 'echo' => false]) !!}
+      </nav>
+    </div>
+  </div>
   @endif
 
   <div class="cta">
-    <a class="bg-black hover:bg-white border border-white hover:border hover:border-black inline-flex justify-center items-center py-2 px-6 text-base font-medium text-center text-white hover:text-black" href="#">Donate</a>
+    <?php 
+      $link = get_field('button', 'option');
+      if( $link ): 
+        $link_url = $link['url'];
+        $link_title = $link['title'];
+        $link_target = $link['target'] ? $link['target'] : '_self';
+        ?>
+        <a class="bg-black hover:bg-white border border-white hover:border hover:border-black inline-flex justify-center items-center py-2 px-6 text-base font-medium text-center text-white hover:text-black" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+      <?php endif; ?>
   </div>
 </header>
